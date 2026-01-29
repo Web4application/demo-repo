3c0d6e72ec89c5ff78d73c951cc0668ed8d11193
Seriki yakub <167559384+Web4application@users.noreply.github.com>
2026-01-29 23:21:33 +0100

    Update and rename .env.py to brew.sh

--- .env.py
+++
@@ -1,5 +0,0 @@
-# .env.py
-
-API_URL = "https://nextjs-boilerplate-web4application.vercel.app"
-DEBUG = True
-PROJECT_NAME = "Web4"
---
+++ brew.sh
@@ -0,0 +1,3 @@
+# .env.pyAPI_URL = "https://nextjs-boilerplate-web4application.vercel.app"
+DEBUG = True
+PROJECT_NAME = "Web4"
